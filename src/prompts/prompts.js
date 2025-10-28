9177ba7be29257acab3adc770a287405b5a31686
Kamil Homernik <kamilhomer@gmail.com>
2025-10-28 18:56:16 +0100

    prompts

--- src/prompts/prompts.js
+++ src/prompts/prompts.js
@@ -2,10 +2,10 @@ export const generateTitlePrompt = (
   noteContent
 ) => `Napisz mi tytuł dla następującej notatki: ${noteContent}.
         Maksymalnie 3 wyrazy.
-        Przykłady dobrych tematów: "Notatki ze spotkania", "Lista zakupów", "Ksiązki i blogi"`
+        Przykłady dobrych tematów: "Notatki ze spotkania", "Lista zakupów", "Ksiązki i blogi". Zwracaj tylko wygenerowany tytuł i nic więcej.`
 
 export const generateTagPrompt = (
   noteContent
 ) => `Wygeneruj tag dla notatki: ${noteContent}.
         Tylko 1 wyraz.
-        Przykłady dobrych tagów: "Osoby", "Notatki", "Życiowe", "Nauka"`
+        Przykłady dobrych tagów: "Osoby", "Notatki", "Życiowe", "Nauka". Zwracaj tylko wygenerowany tytuł i nic więcej.`
